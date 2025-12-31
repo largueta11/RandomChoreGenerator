562d358779b5a74aa323461d3c3070b68e962c73
Largueta <largueta11@gmail.com>
2025-12-31 09:09:25 -0600

    Rename index.js to script.js and create array variables for message components

---
+++ script.js
@@ -0,0 +1,10 @@
+let people = ['Luis', 'Jessica', 'Eleanor'];
+
+let recurrences = ['Hourly', 'Daily', 'Weekly', 'Monthly', 'Yearly'];
+
+let intervals = [];
+
+let rooms = ['Kitchen', 'Dining Room', 'Living Room', 'Master Bedroom', 'Eleanors Bedroom', 'Playroom', 'Master Bathroom', 'Eleanors Bathroom', 'Half Bath', 'Master Closet', 'Eleanors Closet', 'Playroom Closet', 'Harry Potter Closet', 'Laundry Room', 'Garage'];
+
+let actions = ['Dust', 'Sweep', 'Mop', 'Vacuum', 'Clean']
+
