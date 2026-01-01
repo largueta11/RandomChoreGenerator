a8b4de13859c2f1eae8d0c4b95266185bdfc0f75
Largueta <largueta11@gmail.com>
2026-01-01 12:59:26 -0600

    Create getRandomElement and buildMessage functions and console.log

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+//---------- Array components
+
 let people = ['Luis', 'Jessica', 'Eleanor'];
 
-let recurrences = ['Hourly', 'Daily', 'Weekly', 'Monthly', 'Yearly'];
-
-let intervals = [];
+let actions = ['Dust', 'Sweep', 'Mop', 'Vacuum', 'Clean'];
 
 let rooms = ['Kitchen', 'Dining Room', 'Living Room', 'Master Bedroom', 'Eleanors Bedroom', 'Playroom', 'Master Bathroom', 'Eleanors Bathroom', 'Half Bath', 'Master Closet', 'Eleanors Closet', 'Playroom Closet', 'Harry Potter Closet', 'Laundry Room', 'Garage'];
 
-let actions = ['Dust', 'Sweep', 'Mop', 'Vacuum', 'Clean']
+let recurrences = ['Day', 'Week', 'Month', 'Year'];
 
+let intervals = [];
+
+//---------- Functions
+
+const getRandomElement = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+const buildMessage = (p, a, r, rec) => {
+    let person = getRandomElement(p);
+    let action = getRandomElement(a);
+    let room = getRandomElement(r);
+    let recurrence = getRandomElement(rec);
+
+    return `${person} will ${action} the ${room} every ${recurrence}.`;
+}
+
+//---------- Main code
+
+console.log(buildMessage(people, actions, rooms, recurrences));
